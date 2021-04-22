9db75eb3cc0900a8562ba7a6a272b692d0b1cc48
Gezim Basha <gezim@moment.ag>
2021-04-22 22:17:51 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"v"},
+		Usage:   "prints the current installed version",
+	}
+
+	app := &cli.App{
+		Name:     "ebliss",
+		HelpName: "ebliss",
+		Usage:    "An event-driven enterprise resource planning application",
+	}
+	app.Version = "v0.1"
+	app.EnableBashCompletion = true
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
